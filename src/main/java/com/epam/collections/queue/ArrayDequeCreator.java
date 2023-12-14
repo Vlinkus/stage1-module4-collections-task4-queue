aa0bf396c5537fea1a65d7bddaab857aea5cbfe6
VLinkus <LinkusVyte@gmail.com>
2023-12-14 19:27:43 +0200

    M4 Collections Task 4 Completed

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,57 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> deq = new ArrayDeque<>();
+        boolean firstNow = true;
+        int count = 0;
+        Integer removed;
+        while(count < 4){
+            if(count<2){
+                count++;
+                removed = firstQueue.poll();
+                if(removed != null) {
+                    deq.offer(removed);
+                }
+            } else {
+                count++;
+                removed = secondQueue.poll();
+                if(removed != null) {
+                    deq.offer(removed);
+                }
+            }
+        }
+        count = 0;
+        while(!secondQueue.isEmpty() ){
+            if(firstNow){
+                if(count == 0){
+                    firstQueue.offer(deq.pollLast());
+                }
+                count++;
+                removed = firstQueue.poll();
+                if(removed != null) {
+                    deq.offer(removed);
+                }
+                if(count == 2){
+                    firstNow = false;
+                    count =  0;
+                }
+            }else {
+                if(count == 0){
+                    secondQueue.offer(deq.pollLast());
+                }
+                count++;
+                removed = secondQueue.poll();
+                if(removed != null) {
+                    deq.offer(removed);
+                }
+                if(count == 2){
+                    firstNow = true;
+                    count =  0;
+                }
+            }
+
+        }
+        return deq;
     }
+
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,30 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> newList = new ArrayList<>();
+        ArrayList<Integer> dishes = new ArrayList<>();
+        if(numberOfDishes > 0) {
+            for (int i = 1; i <= numberOfDishes; i++)
+                dishes.add(i);
+            int count = 0;
+            while (newList.size() != numberOfDishes)
+                for (int i = 0; i < dishes.size(); i++) {
+                    count++;
+                    if (count == everyDishNumberToEat) {
+                        if(i == dishes.size()-1) {
+                            count = 0;
+                        }else {
+                            count = 1;
+                        }
+                        newList.add(dishes.get(i));
+                        dishes.remove(i);
+                    }
+                }
+        }
+        return newList;
     }
 }
